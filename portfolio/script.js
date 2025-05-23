affa13cbd0dc2ce663bcfd52a2c22760ab304e0c
Gabriel Ortiz <gabriel.ortizbatera2998@gmail.com>
2025-05-23 15:05:04 -0300

    feat: adiciona foto do Gabriel ao portfolio

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,9 +1,11 @@
-function toggleCard(cardElement) {
-    const para = cardElement.querySelector("p");
-    if (para.classList.contains("hidden")) {
-      para.classList.remove("hidden");
-    } else {
-      para.classList.add("hidden");
-    }
-  }
+// function toggleCard(cardElement) {
+//     const para = cardElement.querySelector("p");
+//     if (para.classList.contains("hidden")) {
+//       para.classList.remove("hidden");
+//     } else {
+//       para.classList.add("hidden");
+//     }
+//   }
   
+
+console.log("Portfólio carregado!");
